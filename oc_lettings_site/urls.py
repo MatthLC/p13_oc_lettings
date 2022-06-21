ed9c561cc48eb114435a8abb10bf74f16cdf8a60
Matth <gplayxz@gmail.com>
2022-06-21 12:36:59 +0200

    correctif

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -4,14 +4,10 @@ from django.urls import path, include
 from . import views
 
 
-def trigger_error(request):
-    return 1 / 0
-
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
-    path('sentry-debug/', trigger_error),
+    path('sentry-debug/', views.trigger_error),
 ]
