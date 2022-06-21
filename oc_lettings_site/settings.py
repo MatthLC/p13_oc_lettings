306c4d6a647986793da1d7c5007f5c0c9d6d325a
Matth <gplayxz@gmail.com>
2022-06-21 17:02:27 +0200

    admin template

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'how_are_you_?')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'oc-lettings-1337.herokuapp.com']
 
