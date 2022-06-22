7ec06c8c1fa493484fcaefe48e467250fe280ff2
Matth <gplayxz@gmail.com>
2022-06-22 19:16:46 +0200

    ajout env var debug

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -7,8 +7,8 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('lettings/', include('lettings.urls')),
